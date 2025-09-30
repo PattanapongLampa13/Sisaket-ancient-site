df264eef3ae4e17dce0690bfeff98092c6d1b1ff
Pattanapong Lampa <pattanapong.pup@gmail.com>
2025-09-30 17:07:40 +0700

    Del too many

--- vercel/context_processors.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-
-def global_settings(request):
-    duration_seconds = getattr(settings, 'CUSTOM_SESSION_DURATION_SECONDS', 3600)
-    return {
-        'CUSTOM_SESSION_DURATION_MS': duration_seconds * 1000,
-        'api_key': getattr(settings, 'GOOGLE_MAPS_API_KEY', ''),
-    }
