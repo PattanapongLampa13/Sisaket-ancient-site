48c814f8687ec865280725b21b3261e94762d49c
Pattanapong Lampa <pattanapong.pup@gmail.com>
2025-09-30 18:50:45 +0700

    build.sh fix 2

--- build.sh
+++ build.sh
@@ -11,8 +11,3 @@ python3.9 -m pip install -r requirements.txt
 
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
-#!/bin/bash
-set -o errexit
-
-pip install -r requirements.txt
-python manage.py collectstatic --no-input
