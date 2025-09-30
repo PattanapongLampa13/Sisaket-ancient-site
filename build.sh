738764bf14139c40ca545e3547592bf12f7a0645
Pattanapong Lampa <pattanapong.pup@gmail.com>
2025-09-30 18:49:34 +0700

    build.sh fix

--- build.sh
+++ build.sh
@@ -3,9 +3,16 @@
 # Exit on error
 set -o errexit
 
+# Clean previous builds
+rm -rf staticfiles
 # Build the project
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
+#!/bin/bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --no-input
